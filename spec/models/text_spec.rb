31941da132b57be9ebfe471ab9730526e3632f53
bruno montebello <brunormontebello@hotmail.com>
2020-05-13 18:40:27 -0300

    Config FactoryBot for DRY TDD code

--- spec/models/text_spec.rb
+++ spec/models/text_spec.rb
@@ -2,32 +2,35 @@ require 'rails_helper'
 
 RSpec.describe Text, type: :model do
 
-  it 'should test that the factory is valid' do
-    expect(FactoryBot.build :text).to be_valid
-  end
+  describe 'testing #validations' do
 
-  it 'should validate the presence of a title' do
-    text = FactoryBot.build :text, title: '' #empty title
-    expect(text).not_to be_valid
-    expect(text.errors.messages[:title]).to include("can't be blank")
-  end
+    it 'should test that the factory is valid' do
+      expect(build :text).to be_valid
+    end
 
-   it 'should validate the presence of a content' do
-    text = FactoryBot.build :text, content: ''
-    expect(text).not_to be_valid
-    expect(text.errors.messages[:content]).to include("can't be blank")
-  end
+    it 'should validate the presence of a title' do
+      text = build :text, title: '' #empty title
+      expect(text).not_to be_valid
+      expect(text.errors.messages[:title]).to include("can't be blank")
+    end
 
-  it 'should validate the presence of a slug' do
-    text = FactoryBot.build :text, slug: ''
-    expect(text).not_to be_valid
-    expect(text.errors.messages[:slug]).to include("can't be blank")
-  end
+    it 'should validate the presence of a content' do
+      text = build :text, content: ''
+      expect(text).not_to be_valid
+      expect(text.errors.messages[:content]).to include("can't be blank")
+    end
 
-  it 'should validate uniqueness of the slug' do # Slug is the unique identifying part of a web address, typically at the end of the URL
-    text = FactoryBot.build :text
-    invalid_text = FactoryBot.build :text, slug: text.slug
-    expect(invalid_text).not_to be_valid
+    it 'should validate the presence of a slug' do
+      text = build :text, slug: ''
+      expect(text).not_to be_valid
+      expect(text.errors.messages[:slug]).to include("can't be blank")
+    end
+
+    it 'should validate uniqueness of the slug' do # Slug is the unique identifying part of a web address, typically at the end of the URL
+      text = create :text
+      invalid_text = FactoryBot.build :text, slug: text.slug
+      expect(invalid_text).not_to be_valid
+    end
   end
 
 end
