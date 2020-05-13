4ec4e0cc4ef0eecc43cad4e09ac1d0630572edeb
bruno montebello <brunormontebello@hotmail.com>
2020-05-13 18:26:07 -0300

    Add validations for Test model + TDD for Text attributes

--- spec/models/text_spec.rb
+++ spec/models/text_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Text, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it 'should test that the factory is valid' do
+    expect(FactoryBot.build :text).to be_valid
+  end
+
+  it 'should validate the presence of a title' do
+    text = FactoryBot.build :text, title: '' #empty title
+    expect(text).not_to be_valid
+    expect(text.errors.messages[:title]).to include("can't be blank")
+  end
+
+   it 'should validate the presence of a content' do
+    text = FactoryBot.build :text, content: ''
+    expect(text).not_to be_valid
+    expect(text.errors.messages[:content]).to include("can't be blank")
+  end
+
+  it 'should validate the presence of a slug' do
+    text = FactoryBot.build :text, slug: ''
+    expect(text).not_to be_valid
+    expect(text.errors.messages[:slug]).to include("can't be blank")
+  end
+
+  it 'should validate uniqueness of the slug' do # Slug is the unique identifying part of a web address, typically at the end of the URL
+    text = FactoryBot.build :text
+    invalid_text = FactoryBot.build :text, slug: text.slug
+    expect(invalid_text).not_to be_valid
+  end
+
 end
